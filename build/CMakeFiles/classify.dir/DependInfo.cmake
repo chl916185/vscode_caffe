e659bfb91fc532ffd37554bcdc9ab3f96f4acc99
chl916185 <2295764512@qq.com>
2019-02-11 17:01:41 +0800

    caffe_test

---
+++ build/CMakeFiles/classify.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chl/workspaces/torch_cc/vs_code_learn/2/vscode_caffe/main.cpp" "/home/chl/workspaces/torch_cc/vs_code_learn/2/vscode_caffe/build/CMakeFiles/classify.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/chl/workspaces/caffe/include"
+  "/usr/local/include"
+  "/home/chl/workspaces/caffe/build/src"
+  "/usr/local/cuda/include"
+  "/usr/local/include/opencv"
+  "/home/chl/anaconda2/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
